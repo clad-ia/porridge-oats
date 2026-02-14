730d8f852c8b4a0b35a1d2caea5303b41de36375
CaptainDrewBoy <captain.me@outlook.com>
2026-02-14 13:12:34 +0000

    Made progress on tinder scene with scene switching :3

--- left_arrow.gd
+++ left_arrow.gd
@@ -1,5 +1,6 @@
 extends TextureButton
 
+@export_file("*.tscn") var next_scene
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -7,8 +8,7 @@ func _ready() -> void:
 
 # Called whenever button pressed
 func _button_pressed() -> void:
-	pass
-
+	get_tree().change_scene_to_file(next_scene)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
