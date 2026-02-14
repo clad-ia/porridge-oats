882b99cfe1f8be880e94e60643ff80529043a51c
J-Chapps <josephchapple06@gmail.com>
2026-02-14 18:18:13 +0000

    Working card imagesgit add .git add .git add .git add .!

--- cards_panel.gd
+++ cards_panel.gd
@@ -1,57 +1,103 @@
 extends Panel
 
-var card_compliment = preload("res://textures/card_compliment.png")
-#var car_funstory_image
+var card_compliment_image = preload("res://textures/card_compliment.png")
+var card_funstory_image = preload("res://textures/card_compliment.png")
+var card_coolstory_image = preload("res://textures/card_compliment.png")
+var card_getsocials_image = preload("res://textures/card_compliment.png")
+var card_boldgesture_image = preload("res://textures/card_compliment.png")
+var card_makeamove_image = preload("res://textures/card_compliment.png")
 
 
 var allCards = [
 	["Card name", "Tp", "Connection", "Confidence", "Aura", "Type", "image", "location"],
-	["Compliment", "1", "5", "0", "0", "basic",card_compliment, " "],
-	["Fun Story", "1", "0", "5", "0", "basic", " ", " "],
-	["Cool Story", "1", "0", "10", "0", "basic", " "," "],
-	["Get Socials", "2", "5", "5", "5", "basic", " ", " "],
-	["Bold Festure", "2", "0", "15", "0", "basic", " ", " "],
-	["Make a move", "3", "20", "0", "10", "basic",  " ", " "],
+	["Compliment", "1", "5", "0", "0", "basic",card_compliment_image, " "],
+	["Fun Story", "1", "0", "5", "0", "basic", card_funstory_image, " "],
+	["Cool Story", "1", "0", "10", "0", "basic", card_coolstory_image," "],
+	["Get Socials", "2", "5", "5", "5", "basic", card_getsocials_image, " "],
+	["Bold Gesture", "2", "0", "15", "0", "basic", card_boldgesture_image, " "],
+	["Make a move", "3", "20", "0", "10", "basic",  card_makeamove_image, " "],
 	]
 	
 
-
-#create the card deck
+#create the player's deck
 var cardDeck = [
-	
-	["Card name", "Tp", "Connection", "Confidence", "Aura", "Type", "image"],
-	["Card name", "Tp", "Connection", "Confidence", "Aura", "Type", "image"],
+	["Card name", "Tp", "Connection", "Confidence", "Aura", "Type", "image", "location"],
 ]
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var newCard = ["New Card name", "Tp", "Connection", "Confidence", "Aura", "Type"]
-	_card_deck(null)
-	_card_deck(newCard)
+	
+	# build cardDeck
+	for i in range(3):
+		cardDeck.append(allCards[1])
+		cardDeck.append(allCards[2])
+		cardDeck.append(allCards[3])
+	
+	cardDeck.append(allCards[4])
+	cardDeck.append(allCards[5])
+	cardDeck.append(allCards[6])
 	
 	pass # Replace with function body.
 
 # function for adding and getting the card deck
 func _card_deck(newCard):
 	
+	# adds a new card to the deck
 	if newCard != null:
 		cardDeck.append(newCard)
 
 	pass # Replace with function body.
 
 func _load_shop_cards():
-	# shop cards
 	
+	# shop cards
 	# card 1
 	var card_1_chosen = allCards[1]
-	#var tex_1 = ImageTexture.new()
-	#tex_1.create_from_image(card_compliment)
+	$CardShopPanel/CardShopContainer/Card1Shop.texture_normal = card_1_chosen[6]
 	
-	$CardShopPanel/CardShopContainer/Card1Shop.texture_normal = card_compliment
-
+	# card 2
+	var card_2_chosen = allCards[2]
+	$CardShopPanel/CardShopContainer/Card2Shop.texture_normal = card_2_chosen[6]
+	
+	# card 3
+	var card_3_chosen = allCards[3]
+	$CardShopPanel/CardShopContainer/Card3Shop.texture_normal = card_3_chosen[6]
+	
+	pass
+	
+func _load_deck_cards():
+	
+	# shop cards
+	# card 1
+	var card_1_chosen = cardDeck[1]
+	$CardContainer/Card.texture_normal = card_1_chosen[6]
+	
+	# card 2
+	var card_2_chosen = cardDeck[2]
+	$CardContainer/Card.texture_normal = card_2_chosen[6]
+	
+	# card 3
+	var card_3_chosen = cardDeck[3]
+	$CardContainer/Card.texture_normal = card_3_chosen[6]
+	
+	# card 4
+	var card_4_chosen = cardDeck[4]
+	$CardContainer/Card.texture_normal = card_3_chosen[6]
+	
+	# card 5
+	var card_5_chosen = cardDeck[5]
+	$CardContainer/Card.texture_normal = card_3_chosen[6]
 	
 	pass
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _card_1_shop_pressed() -> void:
+	print(cardDeck)
+	pass # Replace with function body.
+
+func _card_2_shop_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _card_3_shop_pressed() -> void:
+	pass # Replace with function body.
